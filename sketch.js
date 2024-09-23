1119ed1d30fd7f7d92ec512aa3086346a13f69fe
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-22 20:20:15 -0400

    first slider

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+let d = 60;
+let deg = 58;
+let outline = true;
+
+let gui;
+let diameterSlider , degSlider, outlineCheckbox;
+
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  strokeWeight(10);
+
+  gui = createGui();
+  diameterSlider = createSlider("Diameter", 200,10, 100, 20,10, 100 );
 }
 
 function draw() {
   background(125);
+  drawGui();
+
+  if (outline) {
+    stroke("black");
+  } else {
+    noStroke();
+  }
+
+
+circle(100, 200, diameterSlider.val);
+
+push();
+translate(200,200);
+rotate (radians(deg));
+rect(0,0,60,60);
+pop();
+
+noStroke();
+text(diameterSlider.label, 140, 20);
 }
